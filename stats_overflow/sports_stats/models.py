97ca7bf57ed1ecac1bb290d229a20f9439481a8e
ColinCampbell <149631324+CColin5@users.noreply.github.com>
2024-04-17 20:20:40 -0500

    Added where you can see the user ID
    We will delete this later

--- stats_overflow/sports_stats/models.py
+++ stats_overflow/sports_stats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def show_user_id(request):
+    user = request.user
+    if user.is_authenticated:
+        return HttpResponse(f"Your user ID is: {user.id}")
+    else:
+        return HttpResponse("You are not logged in.")
