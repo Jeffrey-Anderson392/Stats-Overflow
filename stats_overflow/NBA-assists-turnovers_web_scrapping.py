287093fc6bb77bdc03205677e3120f2b218ae4f6
abmackenthun <159589403+abmackenthun@users.noreply.github.com>
2024-05-01 09:45:48 -0500

    Update NBA-assists-turnovers_web_scrapping.py

--- stats_overflow/NBA-assists-turnovers_web_scrapping.py
+++ stats_overflow/NBA-assists-turnovers_web_scrapping.py
@@ -8,12 +8,13 @@ result = requests.get(url)
 doc = BeautifulSoup(result.content, "html.parser")
 tbody = doc.find("tbody")
 trs = tbody.find_all("tr")
+id_counter =1
 
 with open('NBA-Assists-Turnovers.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
     header = ['PlayerName', 'GamesPlayed', 'GamesStarted', 'TotalAssists', 'AssistsPerGame', 'Turnover', 'TurnoversPerGame', 'AssistsPerTurnover']
     thewriter.writerow(header)
-    id_counter =1
+    
 
     for tr in trs:
         tds = tr.find_all("td")
